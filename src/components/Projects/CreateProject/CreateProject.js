9a2bf79d3c3c0d518767b7a985b5c23abce743aa
Anshul5678 <anshulmeshram22@gmail.com>
2024-12-14 13:34:45 +0530

    create project

--- src/components/Projects/CreateProject/CreateProject.js
+++ src/components/Projects/CreateProject/CreateProject.js
@@ -6,9 +6,9 @@ import { addDoc, collection, doc, Timestamp, getDocs } from "firebase/firestore"
 import { db } from "../../../firebase";
 
 function CreateProject() {
-  const navigate = useNavigate();
   const userDetails = useSelector((state) => state.users);
   const companyDetails = userDetails.companies[userDetails.selectedCompanyIndex];
+  const navigate = useNavigate();
 
   const [projectForm, setProjectForm] = useState({
     status: "On-Going",
@@ -21,9 +21,14 @@ function CreateProject() {
 
   const [isMoreChecked, setIsMoreChecked] = useState(false);
   const [books, setBooks] = useState([]);
-  const [selectedBookId, setSelectedBookId] = useState("");
+  const [formData, setFormData] = useState({ book: { bookRef: { id: "" } } });
 
-  const handleBookSelect = (e) => setSelectedBookId(e.target.value);
+  const onSelectBook = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      book: { bookRef: { id: e.target.value } },
+    }));
+  };
 
   const handleInputChange = (field, value) => {
     setProjectForm((prev) => ({ ...prev, [field]: value }));
@@ -61,12 +66,7 @@ function CreateProject() {
 
   async function fetchBooks() {
     try {
-      const bookRef = collection(
-        db,
-        "companies",
-        companyDetails.companyId,
-        "books"
-      );
+      const bookRef = collection(db, "companies", companyDetails.companyId, "books");
       const getBookData = await getDocs(bookRef);
       const fetchBooks = getBookData.docs.map((doc) => ({
         id: doc.id,
@@ -87,108 +87,131 @@ function CreateProject() {
       <header className="items-center my-2">
         <div className="flex space-x-3">
           <Link
+            className="flex items-center bg-gray-300 text-gray-700 py-1 px-4 rounded-full transform hover:bg-gray-400 hover:text-white transition duration-200 ease-in-out"
             to="/projects"
-            className="flex items-center bg-gray-300 text-gray-700 py-1 px-4 rounded-full hover:bg-gray-400 hover:text-white transition"
           >
             <AiOutlineArrowLeft className="w-5 h-5 mr-2" />
           </Link>
           <h1 className="text-2xl font-bold">Create Project</h1>
         </div>
       </header>
-
       <div className="p-6">
         <div className="bg-white p-4 rounded-lg">
-          <label className="block text-lg text-gray-600 mb-2">
-            Project Name <span className="text-red-500">*</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Project Name"
-            className="border p-2 rounded w-full"
-            value={projectForm.name}
-            onChange={(e) => handleInputChange("name", e.target.value)}
-          />
-
-          <div className="grid grid-cols-2 gap-4 mt-5">
-            <div>
-              <label className="block text-gray-600 mb-1">Start Date</label>
-              <input
-                type="date"
-                className="border p-2 rounded w-full"
-                onChange={(e) => handleDateChange("startDate", e.target.value)}
-              />
-            </div>
-
-            <div>
-              <label className="block text-gray-600 mb-1">Due Date</label>
-              <input
-                type="date"
-                className="border p-2 rounded w-full"
-                onChange={(e) => handleDateChange("dueDate", e.target.value)}
-              />
+          <div className="">
+            <label className="text-lg text-gray-600">
+              Project Name <span className="text-red-500">*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Project Name"
+              className="text-base text-gray-900 font-semibold border p-1 rounded w-full mt-1"
+              value={projectForm.name}
+              onChange={(e) =>
+                handleInputChange("name", e.target.value)
+              }
+              required
+            />
+          </div>
+          <div className="my-5">
+            <h2 className="mb-2">Timelines</h2>
+            <div className="grid grid-cols-2 gap-4 rounded-lg">
+              <div>
+                <label className="text-gray-600">Start Date</label>
+                <input
+                  type="date"
+                  className="border p-1 rounded w-full mt-1"
+                  value={projectForm.startDate || ""}
+                  onChange={(e) =>
+                    handleDateChange("startDate", e.target.value)
+                  }
+                />
+              </div>
+              <div>
+                <label className="text-gray-600">Due Date</label>
+                <input
+                  type="date"
+                  className="border p-1 rounded w-full mt-1"
+                  value={projectForm.dueDate || ""}
+                  onChange={(e) =>
+                    handleDateChange("dueDate", e.target.value)
+                  }
+                />
+              </div>
             </div>
           </div>
         </div>
-
         <div className="flex justify-between items-center my-4">
-          <span>More Details</span>
-          <label className="relative inline-block w-14 h-8">
-            <input
-              type="checkbox"
-              className="sr-only"
-              checked={isMoreChecked}
-              onChange={(e) => setIsMoreChecked(e.target.checked)}
-            />
-            <span className="absolute inset-0 bg-blue-300 rounded-full peer-checked:bg-blue-500 transition"></span>
-            <span className="absolute top-0 left-0 w-8 h-8 bg-white rounded-full shadow transform peer-checked:translate-x-6 transition"></span>
-          </label>
+          <div>More Details</div>
+          <div>
+            <label className="relative inline-block w-14 h-8">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={isMoreChecked}
+                onChange={(e) => {
+                  setIsMoreChecked(e.target.checked);
+                }}
+              />
+              <span className="absolute cursor-pointer inset-0 bg-[#9fccfa] rounded-full transition-all duration-400 ease-[cubic-bezier(0.175,0.885,0.32,1.275)] peer-focus:ring-2 peer-focus:ring-[#0974f1] peer-checked:bg-[#0974f1]"></span>
+              <span className="absolute top-0 left-0 h-8 w-8 bg-white rounded-full shadow-[0_10px_20px_rgba(0,0,0,0.4)] transition-all duration-400 ease-[cubic-bezier(0.175,0.885,0.32,1.275)] flex items-center justify-center peer-checked:translate-x-[1.6em]"></span>
+            </label>
+          </div>
         </div>
-
         {isMoreChecked && (
           <div className="bg-white p-4 rounded-lg">
-            <label className="block text-lg text-gray-600 mb-2">Bank/Book Details</label>
-            <select
-              value={selectedBookId}
-              onChange={handleBookSelect}
-              className="border p-2 rounded w-full mb-4"
-            >
-              <option value="" disabled>
-                Select Bank/Book
-              </option>
-              {books.map((book, index) => (
-                <option key={index} value={book.id}>
-                  {`${book.name} - ${book.bankName} - ${book.branch}`}
+            <div className="">
+              <label className="text-lg text-gray-600">Bank/Book Details</label>
+              <select
+                value={formData.book.bookRef?.id || ""}
+                onChange={onSelectBook}
+                className="border p-2 rounded w-full mt-1"
+              >
+                <option value="" disabled>
+                  Select Bank/Book
                 </option>
-              ))}
-            </select>
-
-            <label className="block text-lg text-gray-600 mb-2">Location</label>
-            <input
-              type="text"
-              placeholder="Enter Location"
-              className="border p-2 rounded w-full mb-4"
-              value={projectForm.location}
-              onChange={(e) => handleInputChange("location", e.target.value)}
-            />
-
-            <label className="block text-lg text-gray-600 mb-2">Description</label>
-            <input
-              type="text"
-              placeholder="Enter Description"
-              className="border p-2 rounded w-full"
-              value={projectForm.description}
-              onChange={(e) => handleInputChange("description", e.target.value)}
-            />
+                {books.length > 0 &&
+                  books.map((book, index) => (
+                    <option value={book.id} key={index}>
+                      {`${book.name} - ${book.bankName} - ${book.branch}`}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="">
+              <label className="text-lg text-gray-600">Location</label>
+              <input
+                type="text"
+                placeholder="Enter Location"
+                className="text-base text-gray-900 font-semibold border p-1 rounded w-full mt-1"
+                value={projectForm.location}
+                onChange={(e) =>
+                  handleInputChange("location", e.target.value)
+                }
+              />
+            </div>
+            <div className="my-5">
+              <label className="text-lg text-gray-600">Description</label>
+              <input
+                type="text"
+                placeholder="Enter Description"
+                className="text-base text-gray-900 font-semibold border p-1 rounded w-full mt-1"
+                value={projectForm.description}
+                onChange={(e) =>
+                  handleInputChange("description", e.target.value)
+                }
+              />
+            </div>
           </div>
         )}
-
         <div className="mt-6 flex justify-end">
-          <button
-            onClick={handleSubmit}
-            className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
-          >
-            + Create Project
-          </button>
+          <div className="flex gap-2">
+            <button
+              className="bg-blue-500 text-white py-1 px-4 rounded-lg flex items-center gap-1"
+              onClick={handleSubmit}
+            >
+              <span className="text-lg">+</span> Create Project
+            </button>
+          </div>
         </div>
       </div>
     </div>
