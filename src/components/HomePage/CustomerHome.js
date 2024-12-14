cf3ae6744a1633ac63f862eb6c3a22f87aa2ef68
Rakesh <chipparakesh01@gmail.com>
2024-12-14 12:28:18 +0530

    dummy customer dashboard

--- src/components/HomePage/CustomerHome.js
+++ src/components/HomePage/CustomerHome.js
@@ -21,16 +21,13 @@ function CustomerHome() {
         </div>
         <div style={{ width: "100%", height: "92vh" }} className="bg-gray-100">
           <Routes>
-            <Route path="/customer/invoice" element={<Invoice />}></Route>
-            <Route path="/customer/quotation" element={<Quotations />}></Route>
-            <Route path="/customer/projects" element={<Projects />}></Route>
-            <Route path="/customer/projects/:id" element={<ProjectView />} />
+            <Route path="/invoice" element={<Invoice />}></Route>
+            <Route path="/quotation" element={<Quotations />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/projects/:id" element={<ProjectView />} />
+            <Route path="/projects/:id/files" element={<Files />}></Route>
             <Route
-              path="/customer/projects/:id/files"
-              element={<Files />}
-            ></Route>
-            <Route
-              path="/customer/projects/:id/approvals"
+              path="/projects/:id/approvals"
               element={<Approval />}
             ></Route>
           </Routes>
